8d7d074118839f0c282da5cae67b282408259717
rodrigosa7 <digex2008@live.com.pt>
2022-03-02 18:09:15 +0000

    Update TaskCheckBox.js

--- gerador-propostas/src/components/TaskCheckBox.js
+++ gerador-propostas/src/components/TaskCheckBox.js
@@ -13,7 +13,7 @@ export const TaskCheckBox = (task) => {
     }
     const handleHours = (e) => {
         setHours(e.target.value);
-    }g
+    }
     const handleCostPrice = (e) => {
         setCostPrice(e.target.value);
     }
