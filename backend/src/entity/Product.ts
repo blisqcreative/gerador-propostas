70c737629d28efe49e7abe6a52e10052899c0eea
rodrigosa7 <rodrigo.gomes.sa@outlook.pt>
2022-05-20 11:21:01 +0100

    Added products

--- backend/src/entity/Product.ts
+++ backend/src/entity/Product.ts
@@ -1,6 +1,11 @@
 import {BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm"
 import {Department} from "./Department"
 
+export enum ProductUnit {
+    UNIDADE = 'unidade',
+    HORA = 'hora',
+}
+
 @Entity()
 export class Product extends BaseEntity {
     @PrimaryGeneratedColumn()
@@ -15,6 +20,15 @@ export class Product extends BaseEntity {
     @Column()
     unit: string;
 
+    @Column({
+        type: 'enum',
+        enum: ProductUnit,
+        default: ProductUnit.HORA
+    })
+    format: ProductUnit;
+
     @ManyToOne(type => Department, department => department.products)
     department: Department;
+
+
 }
